75b0e709c7fb02fbec1220ebabd52bdfbaad89cc
kasumi-sou <110779036+kasumi-sou@users.noreply.github.com>
2025-07-27 00:18:38 +0900

    change: save json file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 import * as cheerio from "cheerio";
 import axios from "axios";
+import * as fs from 'node:fs';
 
 const base = "http://trivia.air-be.net/trivia.php?si=1&so=0&pn=";
 
-const num = 1;
+const pages = 21
 
-const url = `${base}${num}`
+let data = []
 
-const { data: body } = await axios.get(url)
-const $ = cheerio.load(body);
+for(let i = 1; i <= pages; i++) {
+  const url = `${base}${i}`
 
-$("tr").each((i, elem) => {
-  if (i != 0) {
-    const num = $(elem).children("td.no").text().trim();
-    const title = $(elem).children("td.title").text().trim();
-    const hee = $(elem).children("td.number").text().trim();
-    const brain = $(elem).children("td.brain").text().trim();
-    const day = $(elem).children("td.day").text().trim();
+  const { data: body } = await axios.get(url)
+  const $ = cheerio.load(body);
 
-    console.log(num + "/" + title + "/" + hee + "/" + brain + "/" + day);
-  }
-})
+  $("tr").each((i, elem) => {
+    if (i != 0) {
+      const num = $(elem).children("td.no").text().trim().replace("No.", "");
+      const title = $(elem).children("td.title").text().trim();
+      const hee = $(elem).children("td.number").text().trim().replace("へぇ", "");
+      const brain = $(elem).children("td.brain").text().trim();
+      const day = $(elem).children("td.day").text().trim();
+
+      // console.log(num + "/" + title + "/" + hee + "/" + brain + "/" + day);
+
+      const record = {
+        "no": num,
+        "title": title,
+        "hee": hee,
+        "brain": brain,
+        "day": day
+      }
+
+      data.push(record);
+      
+    }
+  })
+}
+
+fs.writeFileSync("trivia.json", JSON.stringify(data, null, 2));
+
+console.log(data);
+
